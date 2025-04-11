ff85b5113edacf6289a9861aec90f2dccd2a72db
krishna03vala <krishnavala311@gmail.com>
2025-04-11 12:04:10 +0530

    Your message describing the changes

--- toppingsnames.js
+++ toppingsnames.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 
@@ -32,5 +33,6 @@ app.post('/parse-toppings', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+  
